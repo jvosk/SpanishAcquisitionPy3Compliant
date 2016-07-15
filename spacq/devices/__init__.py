26b2f6703a8704b0f038ee7078ae017cd551dd9c
Joshua Voskamp <joshuavoskamp@gmail.com>
2016-07-15 13:29:48 -0400

    To be tested

---
+++ spacq/devices/__init__.py
@@ -0,0 +1,2 @@
+from . import agilent, iqc, oxford, rohde_schwarz, tektronix, keithley, cryomagnetics, lakeshore
+manufacturers = [agilent, iqc, oxford, rohde_schwarz, tektronix, keithley, cryomagnetics, lakeshore]
