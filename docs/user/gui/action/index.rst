aff76d173aa6e6bae139a3e8a6b99aef3e631705
Joshua Voskamp <joshuavoskamp@gmail.com>
2016-07-15 13:42:18 -0400

    Ignore the unnecessary files

--- docs/user/gui/action/index.rst
+++
@@ -1,9 +0,0 @@
-######
-Action
-######
-
-.. toctree::
-   :maxdepth: 2
-
-   data_capture
-   smooth_reset
--- spacq/devices/__init__.py
+++
@@ -1,2 +0,0 @@
-from . import agilent, iqc, oxford, rohde_schwarz, tektronix, keithley, cryomagnetics, lakeshore
-manufacturers = [agilent, iqc, oxford, rohde_schwarz, tektronix, keithley, cryomagnetics, lakeshore]
